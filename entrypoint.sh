d25b41399030ad45bb23d40b8fca4c222aa93e0e
Nelson Chen <crazysim@gmail.com>
2019-03-10 10:40:03 -0700

    Remove GitHub Actions in favor of CircleCI

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "REPO: $GITHUB_REPOSITORY"
+echo "ACTOR: $GITHUB_ACTOR"
+
 echo '=================== Install Requirements ==================='
 pip install -r requirements.txt
 echo '=================== Create deploy key to push ==================='
@@ -14,18 +17,19 @@ python generate_meetup_posts.py
 pelican content -o output -s pelicanconf.py
 echo '=================== Publish to GitHub Pages ==================='
 cd output
-remote_repo="git@github.com:${GITHUB_REPOSITORY}.git" && \
-remote_branch="gh-pages" && \
-git init && \
-git remote add deploy "$remote_repo" && \
-git checkout $remote_branch || git checkout --orphan $remote_branch && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-echo -n 'Files to Commit:' && ls -l | wc -l && \
-timestamp=$(date +%s%3N) && \
-git commit -m "Automated deployment to GitHub Pages on $timestamp" > /dev/null 2>&1 && \
-git push deploy $remote_branch --force > /dev/null 2>&1 && \
-rm -fr .git && \
+# shellcheck disable=SC2012
+remote_repo="git@github.com:${GITHUB_REPOSITORY}.git"
+remote_branch="gh-pages"
+git init
+git remote add deploy "$remote_repo"
+git checkout $remote_branch || git checkout --orphan $remote_branch
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+echo -n 'Files to Commit:' && ls -l | wc -l
+timestamp=$(date +%s%3N)
+git commit -m "Automated deployment to GitHub Pages on $timestamp"
+git push deploy $remote_branch --force
+rm -fr .git
 cd ../
 echo '=================== Done  ==================='
