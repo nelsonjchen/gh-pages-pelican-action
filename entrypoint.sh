98f04900d8ba8bbdb166ff7bcb1f053f9d3a1d18
Nelson Chen <crazysim@gmail.com>
2019-03-10 13:03:19 -0700

    Skip CI on pushing to gh-pages

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 echo -n 'Files to Commit:' && ls -l | wc -l
 timestamp=$(date +%s%3N)
-git commit -m "Automated deployment to GitHub Pages on $timestamp"
+git commit -m "[ci skip] Automated deployment to GitHub Pages on $timestamp"
 git push deploy $remote_branch --force
 rm -fr .git
 cd ../
